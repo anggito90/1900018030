0f2713e58be169456788cf13cf8f8c3dbc289a45
anggito90 <anggito1900018030@webmail.uad.ac.id>
2020-06-30 18:48:25 +0700

    Commit for assignment 5

---
+++ 3rd assignment/form.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+Welcome <?php echo (int)$_POST["name"]; ?><br>
+Your address is : <?php echo (int)$_POST["address"]; ?><br>
+Your phone number is : <?php echo (int)$_POST["phone"]; ?><br>
+Your gender is : <?php echo (int)$_POST["gender"]; ?><br>
+Your faculty is : <?php echo (int)$_POST["faculty"]; ?><br>
+Your class is : <?php echo (int)$_POST["class"]; ?><br>
+Your reason to join UAD Photography : <?php echo (int)$_POST["reason"]; ?>
+
+
+
+</body>
+</html> 
